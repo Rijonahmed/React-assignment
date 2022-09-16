f9537ceb79e12569f81cb049684961272e52750d
Rijonahmed <rijon036@gmail.com>
2022-09-17 01:20:37 +0600

    css add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { FcCheckmark } from "react-icons/fc";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <div class="hero bg-base-200">
+        <div class="hero-content flex-col lg:flex-row-reverse">
+          <img src="https://i.ibb.co/8Yk6mmB/vintrica-splash.jpg" class="max-w-sm rounded-lg shadow-2xl" alt='img' />
+          <div>
+            <h1 class="text-3xl">E-vignette: here’s how it works</h1>
+            <p class="py-6">With vintrica it’s easy to register your number plate online for all motorways and main roads that require a vignette. You can simply buy a vignette either from home before you start your journey or on the spur of the moment during your journey on your smartphone before you get on the motorway.</p>
+
+            <p className='flex items-center'><FcCheckmark className='font-bold text-3xl' /> Quick to use</p>
+            <p className='flex items-center'><FcCheckmark className='font-bold text-3xl' /> Instant activation</p>
+            <p className='flex items-center'><FcCheckmark className='font-bold text-3xl' /> Vignette directly on your mobile phone</p>
+            <p className='flex items-center'><FcCheckmark className='font-bold text-3xl' /> Secure online payment</p>
+
+
+
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
